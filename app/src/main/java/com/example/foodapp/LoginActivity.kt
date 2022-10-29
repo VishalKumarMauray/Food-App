368618e6d066d5fcb9794ac18c5349b0042406aa
vishal kumar mauray <vishalmauray18@gmail.com>
2022-10-29 13:48:35 +0530

    login activity complete
    attaching all the activites to login activity

--- app/src/main/java/com/example/foodapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodapp/LoginActivity.kt
@@ -1,11 +1,34 @@
 package com.example.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var login: Button
+    lateinit var registration: TextView
+    lateinit var forgetPsd: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login = findViewById(R.id.btnLogin)
+        registration = findViewById(R.id.txtRegisterYourself)
+        forgetPsd = findViewById(R.id.txtForgotPassword)
+
+        registration.setOnClickListener(){
+            startActivity(Intent(this,RegistrationActivity::class.java))
+        }
+
+        forgetPsd.setOnClickListener(){
+            startActivity(Intent(this,ForgetPswActivity::class.java))
+        }
+
+        login.setOnClickListener(){
+            startActivity(Intent(this,DashboardActivity::class.java))
+        }
     }
 }
